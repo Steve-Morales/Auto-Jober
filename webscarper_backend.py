d25b9af2db19f289c9f01a6e45a5e49a53055cd8
Steve-Morales <sam008@ucsd.edu>
2021-10-24 13:09:25 -0700

    Added comments: backend

--- webscarper_backend.py
+++ webscarper_backend.py
@@ -3,3 +3,12 @@ from pyppeteer import launch
 #to install pyppeteer run:
 #           pip install pyppeteer
 #Windows:   cd c:\\python39\\Scripts
+
+#function: check for autofill
+
+#function: upload resume/coverletter
+#function: autofill
+
+#function: find [submit button] and submit
+
+#function: check/determine if submition was successful
