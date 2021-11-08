56092d8cbf4b7c39ddaf395cad49da40fedba01f
gmmzhouucsd <gmzhou@ucsd.edu>
2021-11-07 16:03:40 -0800

    update front end

--- GUI/auto-jober-gui.py
+++ GUI/auto-jober-gui.py
@@ -1,36 +1,49 @@
 import tkinter as tk
+from tkinter import filedialog
 
 # window dimensions and title
 window = tk.Tk()
 window.title("Auto Jober")
-window.geometry("500x500")
+window.geometry("500x300")
 
-# function for when Start button is clicked
+# function for when Start button is clicked (backend starts running)
 def onclick():
-    print(1) # placeholder
+    print(resume) #placeholder
+
+# function for asking user to upload resume/file
+def uploadResume():
+    resume = filedialog.askopenfilename()
+    print('Selected:', resume)
 
 """"
 prompt = tk.Label(text = "Name")
 prompt.pack()
 dropdown = tk.OptionMenu(window, value, *options)
 """
-input = tk.Entry(window, width=30)
-input.pack()
-input.insert(0, "First name")
 
-input = tk.Entry(window, width=30)
-input.pack()
-input.insert(0, "Last name")
+firstName = tk.Entry(window, width=30)
+firstName.pack()
+firstName.insert(0, "First name")
 
-input = tk.Entry(window, width=30)
-input.pack()
-input.insert(0, "Email Address")
+lastName = tk.Entry(window, width=30)
+lastName.pack()
+lastName.insert(0, "Last name")
 
-input = tk.Entry(window, width=30)
-input.pack()
-input.insert(0, "Phone Number")
+email = tk.Entry(window, width=30)
+email.pack()
+email.insert(0, "Email Address")
 
+phoneNum = tk.Entry(window, width=30)
+phoneNum.pack()
+phoneNum.insert(0, "Phone Number")
 
+jobTitle = tk.Entry(window, width=30)
+jobTitle.pack()
+jobTitle.insert(0, "Desired Job Title")
+
+resume = 0
+button = tk.Button(text="Upload Resume", command=uploadResume)
+button.pack()
 
 button = tk.Button(text="Start", command=onclick)
 button.pack()
