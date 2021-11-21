e133ad5c35ed07ada0a459010c5792d82fb743d9
gmmzhouucsd <gmzhou@ucsd.edu>
2021-11-21 15:22:06 -0800

    getuserinput implementation

--- auto_jober_gui.py
+++ auto_jober_gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import simpledialog
 
 import webscraper_backend
 
@@ -45,7 +46,7 @@ def onclick():
 # lastly, the output of this function should be the text the user typed
 def GetUserInput(question):
     print("Question: ", question)#for debugging, can be deleted
-    answer = "Place Holder Answer"
+    answer = simpledialog.askstring("Input", question)
     print("Answer: ", answer)#for debugging, can be deleted
     return answer
 
