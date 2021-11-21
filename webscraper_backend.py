953b7407b0f309c10eed72f1ccaaf5bc203c8b0a
Steve-Morales <sam008@ucsd.edu>
2021-11-21 13:47:20 -0800

    [Backend]: Added Comments

--- webscraper_backend.py
+++ webscraper_backend.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-#from selenium.webdriver.support import select
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 import time
@@ -16,15 +15,27 @@ jobKeyword = "Software Engineer"    # keyword for related jobs
 # Driver -- Global Variable -- NOT TO BE REASSIGNED
 driver = webdriver.Firefox()
 
+########################## TODO ####################################
+# Make sure this works
+#
 # Determines if the current 'form' being looked at has a 'Next' button 
 # @return True (exists) or False (does not exist)
 def hasNext():
     return len(driver.find_elements_by_xpath('//button[normalize-space()="Next"]')) == 1
 
+########################## TODO ####################################
+# Make sure this works
+#
 # Clicks on a 'Next' button if one exists
 def clickNext():
     driver.find_element_by_xpath('//button[normalize-space()="Next"]').click()
 
+#                           TODO                         #
+# Since we just need something to show at the demo, this
+# isn't too important.
+# Need to allow any keyword(s) to be used to search for 
+# jobs.
+#
 #go the website with set keyowrds and filters 
 ##### GENERALIZE KEYWORDS
 def GoToWebsite():
@@ -42,7 +53,7 @@ def GetJobList():
     html_list = driver.find_element_by_class_name("jobs-search-results");#parent-all the jobs
     return html_list.find_elements_by_tag_name("li")#children-individual jobs
 
-# GET FORM ITEMS (WEB OBJECT CONTAING QUESTIONS AND ANSWERS)
+# GET FORM ITEMS (WEB OBJECT CONTAINING QUESTION AND ANSWER)
 def GetFormItems():
     #Get Form
     form_div = driver.find_element_by_xpath('//*[@id="artdeco-modal-outlet"]')#divider containing form
@@ -66,7 +77,11 @@ def GetAllQuestions(form_items):
                 questions.append(q.text)
     return questions
 
-# Caveat: if there are muliple questions in a particular 'form_item' you can't guarentee you will get all answers 
+# GET ALL ANSWERS FROM FORM
+# dropdown and radio answers are lists within 'answer' list
+# having it this way maintains order 
+# (i.e questions[n] has answers that pertain to answers[n] and vice versa)
+# Note: getting answers from radio buttons need to be tested
 def GetAllAnswers(form_items):
     answers = []
     for item in form_items:#question Answers
@@ -74,8 +89,10 @@ def GetAllAnswers(form_items):
             temp = item.find_element_by_tag_name("input")
             #print(temp.text)
             answers.append(temp)
+            continue
         except NoSuchElementException:
-            print("There is no 'input' tag")
+            pass
+            #print("There is no 'input' tag")#for debugging, can be deleted
         
         try:
             #print("attempting to read drop down\n")
@@ -86,8 +103,10 @@ def GetAllAnswers(form_items):
             if options[0].text == "Select an option":
                 options.remove(options[0])
             answers.append(options)
+            continue
         except NoSuchElementException:
-            print("There is no dropdown")
+            pass
+            #print("There is no dropdown")#for debugging, can be deleted
         
         try: 
             temp = item.find_element_by_class_name("fb-radio-buttons")
@@ -95,8 +114,10 @@ def GetAllAnswers(form_items):
             #for a in options:
             #    print(a.text)
             answers.append(options)
+            continue
         except NoSuchElementException:
-            print("There is no radio buttons")
+            pass#if it gets to this point, then there is a massive error
+            #print("There is no radio buttons")#for debugging, can be deleted
     return answers
 
 # FOR DEBUGGING
@@ -111,37 +132,53 @@ def PrintAnswers(answers):
         except:
             print(a.text)
 
-question_dictionary = []
-answer_dictionary = []
-
+# Using the question from the a form item, we retrieve an answer from the user
+# Using the answer from the user, it selects/inputs their answer for them
 def InputGetAnswer(question):
     return auto_jober_gui.GetUserInput(question)
 
-# web_element is the selected element that is the input box
+# web_element is the input/text box
+# using the answer retrieved from the user, this function types their answer
+# into the textbox
 def InputSetAnswer(answer, web_element):
     web_element.send_keys(answer)
 
+# Assume that options have "acceptable-only" answers 
+# (i.e "Select option" cannot be a choice for the user)
+# This function retrieves and answer from the user
 def DropdownGetAnswer(question, options):
     choices = []
     for option in options:
         choices.append(option.text)
     return auto_jober_gui.GetUserChoice(question, choices)
 
+########################## TODO ####################################
+# There seems to be some sort of bug where it does not choose the
+# correct answer
+#
 # select_web_element is the element that has selectable input(s) (dropdown)
 def DropdownSetAnswer(answer, select_web_element):
     sel = Select(select_web_element)
     sel.select_by_visible_text(answer)
-    select_web_element.send_keys(Keys.RETURN)
+    #select_web_element.send_keys(Keys.RETURN)
     #Select(select_web_element).select_by_index(1)
 
+# Get's questions from the current form item
+# Note: the parameter is not the list of form items,
+#       but an item from the form items.
 def GetQuestion(form_item):
     questions = form_item.find_elements_by_tag_name("span")
     for q in questions:
         if q.text != 'Required':
-            return q.text
-    return q.text
+            return q.text #assumes that there are only two 'span' tag objects
+    return questions.text #assumes that there was only one 'span' tag objects
 
-# asks and inputs answer from user
+########################## TODO ####################################
+# -Radio buttons
+# -Dropdown
+#
+# Retrieves answer from user and inputs answer for user
+# for each item in 'form_items'
 def UpdateInput(item):
     try:
         temp = item.find_element_by_tag_name("input")
@@ -174,8 +211,31 @@ def UpdateInput(item):
         pass
         #print("There is no radio buttons")#for debugging
 
-#APPLY THROUGH EACH JOB
-def ApplyToJobs(): #for the moment we don't want this to run -- eventually we turn this into a function
+# Note: for any i>=0, question[i] matches with answer[i]
+question_dictionary = [] #list of questions already answered
+answer_dictionary = [] #list of answers for each question
+
+########################## TODO ####################################
+# Since we just need something for the demo, this does not have to be
+# effecient or save any data.
+# Note: this may have to come before 'UpdateInput' function
+def UpdateDictionary(question, answer):
+    pass
+
+########################## TODO ####################################
+# Since we just need something for the demo, this does not have to be
+# effecient.
+# This function should return the index of the question
+# if it cannot be found, then it should return -1
+def FindQuestionInDictionary(question):
+    pass
+# Note: I don't think we need a 'FindAnswerInDictionary' function
+
+
+
+# APPLY THROUGH EACH JOB
+# This is where we put everything together
+def ApplyToJobs():
     i=1
     GoToWebsite()
     LogIn()
@@ -195,7 +255,7 @@ def ApplyToJobs(): #for the moment we don't want this to run -- eventually we tu
             UpdateInput(item)
                 
         
-        time.sleep(1000)
+        time.sleep(1000)#remove this if nessesary, should be removed once everything works
         
         while (hasNext()):
             clickNext()
@@ -204,7 +264,7 @@ def ApplyToJobs(): #for the moment we don't want this to run -- eventually we tu
         print(job.text + "\n")
         driver.execute_script("window.scrollTo(0, 100)")
         i += 1
-#from auto_jober_gui import * 
+ 
 #Notes:
 #       *questions and answers are collected from top to bottom, left to right
 #        lists that directly scrape answers or questions are in that order (i.e question[3] corresponds to answer[3]) 
